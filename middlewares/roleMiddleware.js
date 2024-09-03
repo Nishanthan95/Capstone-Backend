d73a220aa6f0d80e30e9e4fc45df04862d1d3679
Nishanthan <ngopalaratnam@invorg.com>
2024-09-03 12:42:44 +0530

    Changes made

--- middlewares/roleMiddleware.js
+++ middlewares/roleMiddleware.js
@@ -1,5 +1,5 @@
 const roleMiddleware = (role) => (req, res, next) => {
-    if (req.user.role !== role) {
+    if (req.user.role !== role && req.user.role !== 'admin') {
         return res.status(403).json({ msg: 'Permission denied' });
     }
     next();
